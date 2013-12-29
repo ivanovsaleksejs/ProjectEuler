cbdea045b8424aa2de994c5f8d8b17f879382016
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2013-12-29 15:42:51 +0200

    Added optimized versions for Problems 1 and 2

--- 001_sum35.hs
+++ 001_sum35.hs
@@ -1,4 +1,11 @@
 nums n = [x|x <- ns, x `mod` 3 == 0 || x `mod` 5 ==0]
     where ns = [1..n]
 
-main = print $ sum $ nums 999
+-- slightly optimized version of previous
+nums' n = [3,6..n] ++ [x|x <- [5,10..n], x `mod`3 /= 0]
+
+-- much more efficient version
+sum' x = (1 + x) * x `div` 2 
+nums'' n = map (\x -> x * sum' (n `div` x)) [3,5,-15]
+
+main = print $ sum $ nums'' 999
