13a297935669d024e33d47f4263cc76e29ef4000
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-01-02 04:44:40 +0200

    Problem 16

---
+++ 016_power_digit_sum.hs
@@ -0,0 +1,3 @@
+import qualified Euler.Func as F (sumDigits)
+
+main = print . F.sumDigits $ 2^1000
--- Euler/Func.hs
+++ Euler/Func.hs
@@ -1,7 +1,13 @@
 module Euler.Func (
- Euler.Func.sum
+ Euler.Func.sum,
+ Euler.Func.sumDigits
 )
 
 where
 
+import Data.Char
+
 sum x = (1 + x) * x `div` 2 
+
+sumDigits :: Integer -> Int
+sumDigits = Prelude.sum . map digitToInt . show
