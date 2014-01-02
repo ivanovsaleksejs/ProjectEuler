8782be6d81650babbf9158bdac3f1c7a5c7a38a5
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-01-02 04:00:35 +0200

    Separated solutions and added golden ratio version for Problem 2

--- 002_even_fib.hs
+++ 002_even_fib.hs
@@ -8,15 +8,4 @@ sumFibs n
         f  = fib n
         fs = sumFibs (n+1)
 
--- more efficient version as it doesn't use Fib module.
--- Motivation: if we assume that Fibonacci sequence is 1,1,2,3,5,8...
--- then it's easy to find out that only every third number will be even.
--- Thus, we can use odd Fibonacci numbers as arguments.
-sumFibs' x y
-    | f > 4000000 = 0
-    | otherwise   = f + fs
-    where 
-        f  = x + y
-        fs = sumFibs' (f+y) (f*2+y)
-
-main = print $ sumFibs' 1 1
+main = print $ sumFibs 1 
