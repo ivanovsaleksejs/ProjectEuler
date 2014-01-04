0e5469cb43ae7314e7ea415d83ab6fa8419b2e5d
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-01-04 21:38:02 +0200

    Problem 28

---
+++ 028_spiral_diag.hs
@@ -0,0 +1,14 @@
+-- If we analyze spirals with size n
+-- we will see, that ascending diagonal (/) is formed from 
+-- all odd squares and all even squares plus 1.
+-- So we can use formula n*(n+1)*(2n+1)/6 to calculate sum of squares 
+-- and then add amount of even numbers,
+-- which, obviously, is n div 2
+-- As for descending diagonal (\)
+-- we can see, that each element is formed of n^2+(n+1) till n-1.
+-- So this diagonal is a sum of squares 1..n-1 plus sum of range (2..n).
+-- If we combine both formulas, we get this:
+
+diagSum n = (n*(4*n^2 + 3*n + 8) - 6) `div` 6
+
+main = print $ diagSum 1001
