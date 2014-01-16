5e7a74515fd60e57c0866b9186964a5bf8ddfa7f
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-01-16 09:17:29 +0200

    Added isPrime function

--- Euler/Primes.hs
+++ Euler/Primes.hs
@@ -1,18 +1,21 @@
 module Euler.Primes (
  primesST,
  prime,
+ isPrime,
  pfactor
 )
 
 where
 
-primesST = 2 : primes' 
+primesST = 2 : primes'
   where
       primes' = sieve 3 9 primes' 0
       sieve x q ~(_:t) k = let fs = take k primes' in
           filter ((`all` fs) . ((/=0).) . rem) [x,x+2..q-2] ++ sieve (q+2) (head t^2) t (k+1)
 
 prime n = (take (n+1) primesST) !! n
+
+isPrime x = x `elem` (takeWhile (<=x) primesST)
 
 getNext n p
     | p `mod` k == 0 = k
