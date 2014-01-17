584a6eab01deb59cafd8b39455bac42df3e162ac
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-01-18 01:42:57 +0200

    Problem 47

---
+++ 041-050/047_distinct_pfactors.hs
@@ -0,0 +1,11 @@
+import Data.List (group, sort, nub)
+import Euler.Primes (pfactor, pfactor')
+
+find (a:b:c:d:xs)
+    | length (nub fs) == 16 = a
+    | otherwise             = find (b:c:d:xs)
+    where
+        fs = as ++ bs ++ cs ++ ds
+        [as, bs, cs, ds] = map (map product . group . pfactor') [a,b,c,d]
+
+main = print $ find [1..]
